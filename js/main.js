8cc437a42c1ca777ef4f84fd37f9635a1707efc3
John Oliphant <john@practicalchrome.com>
2015-03-03 13:29:09 -0500

    Still working things out... But getting better.

--- js/main.js
+++ js/main.js
@@ -3,8 +3,21 @@ window.onload = function() {
  //  'Hello, World! It is ' + new Date();
 };
 
+
+
+/* function FeedConvert($http) {
+          var factory = [];
+        
+          factory.getTodos = function(){
+            return $http.get("http://cdn.chromebooks.today/rss/feed.xml");
+          }
+        
+            return factory;
+	}; 
+
+function FeedCtrl($scope, $http, FeedConvert) { */
 function FeedCtrl($scope, $http) {
-  var item = {
+/*   var item = {
     face: '/assets/60.jpeg',
     what: 'Brunch this weekend?',
     who: 'Min Li Chan',
@@ -18,18 +31,69 @@ function FeedCtrl($scope, $http) {
       who: "Min Li Chan",
       notes: "I'll be in your neighborhood doing errands."
     });
-  };
+  }; */
   
-    $http.get('http://cdn.chromebooks.today/rss/feed.xml')
+/*     $http.get('http://cdn.chromebooks.today/rss/feed.xml')
     .success(function(response) {
 		$scope.feedData = response;
 		console.log($scope.feedData);
 		var x2js = new X2JS();
         $scope.json = x2js.xml_str2json($scope.feedData);
 		console.log($scope.json);
-	});
+	}); 
 	
-};
+	$scope.todos = [];
+          loadTodos();
+          
+           function loadTodos(){
+            FeedConvert.getTodos().success(function(data){
+			    console.log(data);
+				var x2js = new X2JS()
+                rss  = x2js.xml_str2json(data);
+                console.log(rss);
+                $scope.todos =rss;
+				console.log($scope.todos);
+				
+				$scope.jsontodos = angular.toJson($scope.todos);
+				console.log($scope.jsontodos);
+				console.log($scope.parsed);
+				
+				
+            });
+	
+}; */
+
+  $scope.title = "How to use XML inside AngularJS";
+  $http.get('assets/feed.xml').then(function(response) {
+    var items = [];
+    /*setting up the response*/
+	var x2js = new X2JS()
+    var itemDef = x2js.xml_str2json(response.data);
+    console.log(itemDef);
+    $scope.itemsObj = itemDef.rss.channel.item;
+	console.log($scope.itemsObj);
+	console.log($scope.itemsObj.title);
+ 
+    /*looping through the chapters*/
+    var numOfItems = $scope.itemsObj.length;
+	console.log('numOfItems =' + numOfItems);
+    for (var i = 0; i < numOfItems; i++) {
+      items.push({
+        title: $scope.itemsObj[i].title,
+        link: $scope.itemsObj[i].link,
+		author: $scope.itemsObj[i].author,
+		pubDate: new Date($scope.itemsObj[i].pubDate),
+		summary: $scope.itemsObj[i].summary
+      });
+    }
+ 
+    $scope.itemNames = items;
+	console.log(items);
+	console.log(items[0].title);
+		console.log(items[0].summary.__cdata);
+  });
+
+}; 
 
 var app = angular.module('ctApp', ['ngMaterial'])
 // config from Shaun at StackOverFlow - http://stackoverflow.com/a/22798336
@@ -44,4 +108,4 @@ var app = angular.module('ctApp', ['ngMaterial'])
     }
 ])
 .controller('FeedCtrl', FeedCtrl);
-
+//.factory('FeedConvert', FeedConvert);
