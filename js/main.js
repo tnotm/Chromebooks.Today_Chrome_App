5cfbac08aa5f707a6c173bbec8c78f797152bae2
John Oliphant <john@practicalchrome.com>
2015-03-06 01:57:59 -0500

    page changes working

--- js/main.js
+++ js/main.js
@@ -3,15 +3,17 @@ window.onload = function() {
 
 function FeedCtrl($scope, $http) {
 
-  $http.get('assets/feed.xml').then(function(response) {
+    $scope.itemId = 0;
+
+   $http.get('assets/feed.xml').then(function(response) {
 //	$http.get('http://cdn.chromebooks.today/rss/feed.xml').then(function(response) {
     var items = [];
-	var x2js = new X2JS()
+	  var x2js = new X2JS()
     var itemDef = x2js.xml_str2json(response.data);
     $scope.itemsObj = itemDef.rss.channel.item;
 
     var numOfItems = $scope.itemsObj.length;
-	
+
     for (var i = 0; i < numOfItems; i++) {
       items.push({
         title: $scope.itemsObj[i].title,
@@ -25,9 +27,35 @@ function FeedCtrl($scope, $http) {
     }
     $scope.itemNames = items;
   });
-}; 
 
-var app = angular.module('ctApp', ['ngMaterial'])
+  $scope.itemId = function(index) {
+    $scope.itemId = index;
+    console.log($scope.itemId);
+  };
+
+
+};
+
+function EpisodeCrtl($scope) {
+
+};
+
+var app = angular.module('ctApp', ['ngMaterial', 'ngAnimate', 'ngRoute'])
+.config(function($routeProvider) {
+    $routeProvider
+    	.when('/', {
+    		templateUrl: 'list.html',
+            controller: 'FeedCtrl'
+    	})
+    	.when('/about', {
+    		templateUrl: 'about.html',
+           controller: 'EpisodeCrtl'
+    	})
+    	.when('/contact', {
+    		templateUrl: 'page-contact.html',
+            controller: 'contactController'
+    	});
+})
 // config from Shaun at StackOverFlow - http://stackoverflow.com/a/22798336
 .config( [ '$compileProvider', function( $compileProvider ) {
         var currentImgSrcSanitizationWhitelist = $compileProvider.imgSrcSanitizationWhitelist();
@@ -36,4 +64,6 @@ var app = angular.module('ctApp', ['ngMaterial'])
         +currentImgSrcSanitizationWhitelist.toString().slice(-1);
     }
 ])
-.controller('FeedCtrl', FeedCtrl);
+.controller('FeedCtrl', FeedCtrl)
+.controller('EpisodeCrtl', EpisodeCrtl);
+
